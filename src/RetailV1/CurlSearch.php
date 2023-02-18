e98871048b7db4ed821fb1b49823c45efb01b74b
agung <mr.gomu@gmail.com>
2023-02-18 21:32:00 +0700

    pelita

--- src/RetailV1/CurlSearch.php
+++ src/RetailV1/CurlSearch.php
@@ -9,23 +9,36 @@ class CurlSearch implements CurlStageInterface
 {
     public function getUrl(AirlinesInterface &$obj)
     {
-        return '';
+        return 'https://www.pelita-air.com/home/search_flight';
     }
 
     public function getParams(AirlinesInterface &$obj)
     {
         $params = [
+            'CityFrom' => $obj->data->origin,
+            'CityTo' => $obj->data->dest,
+            'DepartDate' => $obj->data->dptDate,
+            'Adult' => $obj->data->adult,
+            'Child' => $obj->data->child,
+            'Infant' => $obj->data->infant,
+            'PromoCode' => '',
+
         ];
         return $params;
     }
 
     public function getMethod(AirlinesInterface &$obj)
     {
-        return 'post';
+        return 'POST';
     }
 
     public function getHeaders(AirlinesInterface &$obj)
     {
-        return [];
+        return [
+            'authority' => 'www.pelita-air.com',
+            'accept' => 'application/json, text/javascript, */*; q=0.01',
+            'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.46',
+            'content-type' => 'application/x-www-form-urlencoded; charset=UTF-8'
+        ];
     }
 }
--- src/RetailV1/ParseSearch.php
+++ src/RetailV1/ParseSearch.php
@@ -4,11 +4,24 @@ namespace Apzcrawl\PelitaAir\RetailV1;
 
 use Apzcrawl\Airlines\Helpers\StageInterface;
 use Apzcrawl\Airlines\AirlinesInterface;
+use Apzcrawl\Airlines\Flight\FlightCollection;
 
 class ParseSearch implements StageInterface
 {
     public function process(AirlinesInterface &$obj)
     {
-        var_dump($obj->lastRetval->getBody()->getContents());
+        // var_dump(json_decode($obj->lastRetval->getBody()->getContents()));
+        $retval = json_decode($obj->lastRetval->getBody()->getContents());
+        $flightCollection = new FlightCollection($obj->data->origin, $obj->data->dest, $obj->data->adult, $obj->data->child, $obj->data->infant);
+        // print_r($retval->DATA[0]);
+        $data = $retval->DATA[0][0];
+        echo "<br/>";
+        foreach ($data as $key => $value) {
+            echo $key . ' = ' . $value;
+            echo "<br/>";
+        }
+        // // print_r($retval->DATA[0][0]);
+        // echo "<br/>";
+
     }
 }
