6f0653ae21bccd8901eb503b1ed7a91a639e6cc7
agung <mr.gomu@gmail.com>
2023-02-20 04:24:10 +0700

    add

--- tests/crawl.php
+++ tests/crawl.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    '1' => '{"Cmd":"Crawl","Class":"","origin":"CGK","dest":"DPS","adult":"1","child":"1","infant":"1","dptDate":"2023-02-19","rtnDate":"","gmtOrigin":"28800","gmtDest":"25200","cron_job":1202}',
+    '1' => '{"Cmd":"Crawl","Class":"","origin":"CGK","dest":"DPS","adult":"1","child":"1","infant":"1","dptDate":"2023-02-21","rtnDate":"","gmtOrigin":"28800","gmtDest":"25200","cron_job":1202}',
 
 ];
